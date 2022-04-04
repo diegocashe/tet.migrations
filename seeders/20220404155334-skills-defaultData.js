79037e1d0eb6982c0e4fe5d1d7a8771b908a82f3
Diego Castillo <diegocashe17@gmail.com>
2022-04-04 11:59:03 -0400

    third commit

--- seeders/20220404155334-skills-defaultData.js
+++ seeders/20220404155334-skills-defaultData.js
@@ -4,44 +4,39 @@ module.exports = {
   async up(queryInterface, Sequelize) {
     return queryInterface.bulkInsert('Skills', [
       {
-          name: 'HTML',
-          description: 'Estructura de páginas web',
-          createdAt: new Date(),
-          updatedAt: new Date()
+        name: 'HTML',
+        description: 'Estructura de páginas web',
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
-          name: 'CSS',
-          description: 'Estilos para páginas web',
-          createdAt: new Date(),
-          updatedAt: new Date()
+        name: 'CSS',
+        description: 'Estilos para páginas web',
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
-          name: 'BOOTSTRAP',
-          description: 'Framework de estilos para páginas web',
-          createdAt: new Date(),
-          updatedAt: new Date()
+        name: 'BOOTSTRAP',
+        description: 'Framework de estilos para páginas web',
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
-          name: 'Javascript',
-          description: 'Lenguaje de programación para páginas web',
-          createdAt: new Date(),
-          updatedAt: new Date()
+        name: 'Javascript',
+        description: 'Lenguaje de programación para páginas web',
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
-          name: 'Excel',
-          description: 'Aplicación informática para gestion administrativa de bases de datos',
-          createdAt: new Date(),
-          updatedAt: new Date()
+        name: 'Excel',
+        description: 'Aplicación informática para gestion administrativa de bases de datos',
+        createdAt: new Date(),
+        updatedAt: new Date()
       }
-  ]);
+    ]);
   },
 
   async down(queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    return queryInterface.bulkDelete('Skills', null, {});
   }
 };
